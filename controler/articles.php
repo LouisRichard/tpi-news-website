ea89e5bd62ab64ddaee27f23854e50187daf2f53
LouisRichard <louis.richard@tutanota.com>
2022-05-20 14:08:46 +0200

    All tests passing

--- controler/articles.php
+++ controler/articles.php
@@ -39,22 +39,27 @@ function addArticle($request)
 {
     if ($_SESSION['admin'] == 1) {
         $imageDir = "assets/img/articles/";
-        $imageFile = $imageDir . basename($_FILES['articleImage']['name']);
+        $imageFile = $imageDir . basename($_FILES['articleImage']['name']).date('ymdhis');
 
         $abstract = str_replace("'", "\'", $request['abstract']);
         $article = str_replace("'", "\'", $request['article']);
         $article = str_replace("\n", "<br/>", $article);
         $categoryID = $request['category'];
         $author = $request['author'];
-        if ($_FILES['articleImage']['size'] < 5000000) { //check if file size is below 5MB
-            $mimeType = finfo_file(finfo_open(FILEINFO_MIME_TYPE), $_FILES['articleImage']['tmp_name']);
-            $allowedFileTypes = ['image/png', 'image/jpeg', 'image/jpg'];
-            if (in_array($mimeType, $allowedFileTypes)) {
-                if (is_uploaded_file($_FILES["articleImage"]["tmp_name"])) {
-                    if (move_uploaded_file($_FILES['articleImage']['tmp_name'], $imageFile)) {
-                        require_once "model/articlesManager.php";
-                        if (addArticleInDB($abstract, $article, $categoryID, $imageFile, $author)) {
-                            header('Location: index.php?action=home');
+        if (!empty($abstract) && !empty($article) && !empty($categoryID) && !empty($author) && !empty(basename($_FILES['articleImage']['name']))) {
+            if ($_FILES['articleImage']['size'] < 5000000) { //check if file size is below 5MB
+                $mimeType = finfo_file(finfo_open(FILEINFO_MIME_TYPE), $_FILES['articleImage']['tmp_name']);
+                $allowedFileTypes = ['image/png', 'image/jpeg', 'image/jpg'];
+                if (in_array($mimeType, $allowedFileTypes)) {
+                    if (is_uploaded_file($_FILES["articleImage"]["tmp_name"])) {
+                        if (move_uploaded_file($_FILES['articleImage']['tmp_name'], $imageFile)) {
+                            require_once "model/articlesManager.php";
+                            if (addArticleInDB($abstract, $article, $categoryID, $imageFile, $author)) {
+                                header('Location: index.php?action=home');
+                            }
+                        } else {
+                            require_once "model/exceptions/ArticleException.php";
+                            throw new CouldNotSaveFileException("Le fichier n'a pas pu être sauvé. Son nom est peut-être invalide");
                         }
                     } else {
                         require_once "model/exceptions/ArticleException.php";
@@ -62,15 +67,15 @@ function addArticle($request)
                     }
                 } else {
                     require_once "model/exceptions/ArticleException.php";
-                    throw new CouldNotSaveFileException("Le fichier n'a pas pu être sauvé. Son nom est peut-être invalide");
+                    throw new InvalidFileExtensionException("Seulement les fichiers PNG, JPG et JPEG sont supportés");
                 }
             } else {
                 require_once "model/exceptions/ArticleException.php";
-                throw new InvalidFileExtensionException("Seulement les fichiers PNG, JPG et JPEG sont supportés");
+                throw new FileTooHeavyException("Le fichier ne peut faire qu'un maximum de 5Mo");
             }
         } else {
             require_once "model/exceptions/ArticleException.php";
-            throw new FileTooHeavyException("Le fichier ne peut faire qu'un maximum de 5Mo");
+            throw new EmptyArticleFormException("Un ou plusieurs des champs requis sont vides.");
         }
     } else {
         require_once "model/exceptions/LoginException.php";
