224baa583567fda1d7a50933d7c03df3a842052d
LouisRichard <louis.richard@tutanota.com>
2022-05-31 14:09:39 +0200

    Bugfix : redirects to the right page when requested

--- model/articlesManager.php
+++ model/articlesManager.php
@@ -217,11 +217,11 @@ function getArticleComments($articleID)
 function getArticles($catID = null)
 {
     if ($catID != null) {
-        $query = "SELECT abstract, article, image, date, authors.firstname, authors.name
+        $query = "SELECT articles.id, abstract, article, image, date, authors.firstname, authors.name
             FROM articles INNER JOIN authors ON authors.id = articles.Authors_id
             WHERE Category_id = " . $catID . " ORDER BY date DESC LIMIT 20";
     } else {
-        $query = "SELECT abstract, article, image, date, authors.firstname, authors.name
+        $query = "SELECT articles.id, abstract, article, image, date, authors.firstname, authors.name
             FROM articles INNER JOIN authors ON authors.id = articles.Authors_id
             ORDER BY date DESC LIMIT 20";
     }
--- view/category.php
+++ view/category.php
@@ -21,12 +21,12 @@ $title = "TPI - Categorie";
       <div class="col-md-12" data-aos="fade-up">
         <?php foreach ($category as $singleArticle) { ?>
           <div class="d-md-flex post-entry-2 half">
-            <a href="single-post.html" class="me-4 thumbnail">
+            <a href="index.php?action=showArticle&aid=<?=$singleArticle['id']?>" class="me-4 thumbnail">
               <img src="<?=$singleArticle['image']?>" alt="" class="img-fluid">
             </a>
             <div>
               <div class="post-meta"><span class="mx-1">&bullet;</span> <span><?= $singleArticle['date'] ?></span></div>
-              <h3><a href="single-post.html"><?= $singleArticle['abstract'] ?></a></h3>
+              <h3><a href="index.php?action=showArticle&aid=<?=$singleArticle['id']?>"><?= $singleArticle['abstract'] ?></a></h3>
               <p><?=substr($singleArticle['article'], 0, 255)?>...</p>
               <div class="d-flex align-items-center author">
                 <div class="name">
